a900606580b13064228fc2198a998a4be6c748e5
levinahandayani <levinahandayani06@gmail.com>
2020-02-15 13:11:13 +0700

    web-sneaker

---
+++ resources/views/index.blade.php
@@ -0,0 +1,271 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>SNEAKER</title>
+  </head>
+  <body>
+ 
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #5b8c85;">
+  <a class="navbar-brand" href=""></a>
+  <img src="../img/sneaker.png" width="7%" height="7%">
+  <div class="collapse navbar-collapse" id="navbarText">
+    <h2 class="container" style="color: #9be3de;">SNEAKER</h2>
+  </div>
+<form class="form-inline"> 
+    <a href="#" class="btn btn-secondary my-2 my-sm-0 mx-md-3" type="submit">register</a>
+    <a href="#" class="btn btn-secondary my-2 my-sm-0" type="submit">login</a>
+  </form>
+</nav>
+
+
+  <nav aria-label="breadcrumb">
+  <ol class="breadcrumb" style="background-color: #c0ffb3;">
+     <li class="nav-item active" style="background-color: #dddc92;">
+        <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item" style="background-color: #dddc92;">
+        <a class="nav-link " href="#">PRODUK</a>
+      </li>
+      <li class="nav-item" style="background-color: #dddc92;">
+        <a class="nav-link" href="#">CONTACT US</a>
+      </li>
+      <li class="nav-item" style="background-color: #dddc92;">
+        <a class="nav-link" href="#">ABOUT US</a>
+      </li>
+  </ol>
+</nav>
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../img/sneaker1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../img/sneaker2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../img/sneaker3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<br>
+
+
+  <div class="container">
+    <div style="background-color: #ffbaba" width="60%" height="60%">
+    <h1 class="text-center">Produk Terbaru</h1>
+    <div class="row justify-content-center"> 
+  </div>
+  </div>
+
+<div class="container">   
+ <div class="row justify-content-center">
+  <div class="card" style="width: 17rem;" >
+  <img src="../img/compas.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">COMPASS</h5>
+    <p class="card-text">Rp. 1,600,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+  <div class="card" style="width: 17rem;">
+  <img src="../img/converse.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">CONVERSE</h5>
+    <p class="card-text">Rp. 820,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+<div class="card" style="width: 17rem;">
+  <img src="../img/item.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">CONVERSE</h5>
+    <p class="card-text">Rp.700,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+  <div class="card" style="width: 18rem;">
+  <img src="../img/fila.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">FILA</h5>
+    <p class="card-text">Rp.1,500,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+
+
+<div class="container">
+    <div style="background-color: #94d3ac" width="60%" height="60%">
+    <h1 class="text-center mt-5">Produk Diskon</h1>
+    <div class="row justify-content-center"> 
+  </div>
+  </div>
+
+<div class="container">   
+ <div class="row justify-content-center">
+  <div class="card" style="width: 18rem;" >
+  <img src="../img/ferari.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">FERRARI</h5>
+    <p class="card-text">Rp. 800,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+  <div class="card" style="width: 16rem;">
+  <img src="../img/vans.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">VANS</h5>
+    <p class="card-text">Rp. 400,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+<div class="card" style="width: 16rem;">
+  <img src="../img/adidas.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">ADIDAS</h5>
+    <p class="card-text">Rp. 380,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+  <div class="card" style="width: 17rem;">
+  <img src="../img/kodachi.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">KODACHI</h5>
+    <p class="card-text">Rp. 420,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+
+
+<div class="container">
+    <div style="background-color: #a4d7e1" width="60%" height="60%">
+    <h1 class="text-center mt-5">Produk Terlaris</h1>
+    <div class="row justify-content-center"> 
+  </div>
+  </div>
+
+<div class="container">   
+ <div class="row justify-content-center">
+  <div class="card" style="width: 17rem;" >
+  <img src="../img/putih.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">CONVERSE WHITE</h5>
+    <p class="card-text">Rp. 1,200,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+  <div class="card" style="width: 16rem;">
+  <img src="../img/ijo.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">CONVERSE GREEN</h5>
+    <p class="card-text">Rp. 760,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+<div class="card" style="width: 16rem;">
+  <img src="../img/nike.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">NIKE</h5>
+    <p class="card-text">Rp. 1,260,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+  <div class="card" style="width: 16rem;">
+  <img src="../img/reebok.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">REEBOK</h5>
+    <p class="card-text">Rp. 1,500,000.00</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div>
+
+
+<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark">
+
+  <div style="background-color: #6351ce;">
+    <div class="container">
+      <div class="row py-4 d-flex align-items-center">
+
+        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+          <h6 class="mb-0">Get connected with us on social networks!</h6>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container text-center text-md-left" style="background: #21243d; color: white;">
+    <div class="row mt-0">
+
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+        <h6 class="text-uppercase font-weight-bold">Name Products</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>apa ajaa jualll udahh.</p>
+      </div>
+
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+        <h6 class="text-uppercase font-weight-bold">Location</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>jualannya di Tebet pinggirnya pasar inang-inang yang mao belii sokinn ajaa gaessss</p>
+      </div>
+      
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+        <h6 class="text-uppercase font-weight-bold">About US</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+          consectetur
+          adipisicing elit.</p>
+      </div>
+     
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        <h6 class="text-uppercase font-weight-bold">Instagram</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="https://instragram.com/education/"> risti.nurfatiah</a>
+        <p>
+          <a href="https://instragram.com/education/"> legevinaga</a>
+        <p>
+          <a href="https://instragram.com/education/"> risma.julianti</a>
+      </div>
+
+    </div>
+  </div>
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright</div>
+  <!-- Copyright -->
+
+</footer>
+  
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
